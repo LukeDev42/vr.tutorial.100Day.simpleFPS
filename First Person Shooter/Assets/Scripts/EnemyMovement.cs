575a188713054be933f6ed0e94f5a7f4f2c4258f
Luke Leiter <ldlstarwars@gmail.com>
2018-02-27 13:15:07 -0500

    Day 12
    *created a NavMesh
    *can't connect it to the knight
    *created a script to follow the player

---
+++ First Person Shooter/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private NavMeshAgent _nav;
+    private Transform _player;
+
+    private void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        _nav.SetDestination(_player.position);
+    }
+}
