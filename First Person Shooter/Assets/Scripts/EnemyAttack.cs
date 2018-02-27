8f0e1bb6d1abf96417237f570126b6a9bdff72c0
TheDoctor33 <ldlstarwars@gmail.com>
2018-02-27 16:41:37 -0500

    Day 12 cont.
    *need to find a way to make anims not read-only

---
+++ First Person Shooter/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+    Animator _animator;
+    GameObject _player;
+    private bool _collidedWithPlayer;
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool("IsNearPlayer", true);
+        }
+        print("enter trigger with _player");
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject == _player)
+        {
+            _collidedWithPlayer = true;
+        }
+        print("enter collided with _player");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == _player)
+        {
+            _collidedWithPlayer = false;
+        }
+        print("exit collided with _player");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == _player)
+        {
+            _animator.SetBool("IsNearPlayer", false);
+        }
+    }
+
+    void Attack()
+    {
+        if(_collidedWithPlayer)
+        {
+            print("player has been hit");
+        }
+    }
+}
